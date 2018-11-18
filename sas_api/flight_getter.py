6eba3f325c292585257b5c9e7edf47dd7283cb1a
Tomas Bonnedahl <tomas.bonnedahl@trioptima.com>
2018-11-18 18:11:33 +0100

    Added more parsing tests

--- sas_api/flight_getter.py
+++ sas_api/flight_getter.py
@@ -61,6 +61,47 @@ def get_result_from_response(response):
     return None
 
 
+class ResponseParser(object):
+    def __init__(self, response):
+        """
+        :type response: dict
+        """
+        self.response = response
+
+    def parse(self):
+        if 'pricingType' in self.response and self.response['pricingType'] == 'O':
+            # Paid flights only?
+            return None
+
+        if 'outboundFlights' in self.response:
+            outbound = self.response['outboundFlights']
+            for flight_id in outbound:
+                if outbound[flight_id]['stops'] == 0:
+                    if 'isSoldOut' in flight_id:  # TODO: Check value is True also
+                        continue
+
+                    a_date = outbound[flight_id]['startTimeInLocal']
+                    stripped_date = a_date.split('+')[0]
+                    date_t = datetime.strptime(stripped_date, "%Y-%m-%dT%H:%M:%S.%f")
+                    business_seats = 0
+
+                    cabins = outbound[flight_id]['cabins']
+                    if 'BUSINESS' in cabins:
+                        if 'SAS BUSINESS' in cabins['BUSINESS']:
+                            sas_bus = cabins['BUSINESS']['SAS BUSINESS']
+                            if 'products' in sas_bus:
+                                for product_key in sas_bus['products']:
+                                    product = sas_bus['products'][product_key]
+                                    if 'fares' in product:
+                                        for fare in product['fares']:
+                                            if 'avlSeats' in fare:
+                                                business_seats = fare['avlSeats']
+
+                    return LegData(business_seats=business_seats,
+                                   origin=outbound[flight_id]['origin']['code'],
+                                   destination=outbound[flight_id]['destination']['code'],
+                                   date=date_t.date())
+
 def get_flight_info(config):
     delta = config.max_date - config.min_date
     results = []
