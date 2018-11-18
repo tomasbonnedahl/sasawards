e358c7eb1ca2242be438febc1206189506ebd02a
Tomas Bonnedahl <tomas.bonnedahl@trioptima.com>
2018-11-18 11:30:43 +0100

    Added app

--- sasawards/urls.py
+++ sasawards/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import awards.views
+
 urlpatterns = [
+    path("", awards.views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
