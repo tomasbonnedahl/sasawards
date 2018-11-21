4694f39730f61fefc3b63a066739641712b136c9
Tomas Bonnedahl <tomas.bonnedahl@trioptima.com>
2018-11-21 18:18:28 +0100

    Run clock in wsgi.py

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: gunicorn sasawards.wsgi --log-file -
-clock: python clock.py
 worker: python manage.py rqworker default
--- sasawards/wsgi.py
+++ sasawards/wsgi.py
@@ -9,8 +9,31 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sasawards.settings')
+
+import os
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+# sched = BlockingScheduler()
+sched = BackgroundScheduler()
+
+@sched.scheduled_job('interval', minutes=1)  # TODO: Temp
+def timed_job():
+    print('This job is run every one minutes.')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sasawards.settings")
+    import django_rq
+    from sas_api import a_test
+    print('Queuing in wsgi...')
+    django_rq.enqueue(a_test.a_test)
+    print('Queued')
+    # from sas_api.services import fetch_flights
+    # fetch_flights()
+
+print('wsgi')
+sched.start()
 
 application = get_wsgi_application()
