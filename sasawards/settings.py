735b256ab3df6c1b12b268c3f017fe6b40673579
Tomas Bonnedahl <tomas.bonnedahl@trioptima.com>
2018-11-26 20:41:34 +0100

    Move key to env

--- sasawards/settings.py
+++ sasawards/settings.py
@@ -18,9 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# TODO:
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@7bq*xy%z2mdwhdc_i_alxpb-iv79%(xkxyg3q(%)ek3*48nsk'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
