b7c81982cea5ca2437d3d7256088db7574db2e97
Tomas Bonnedahl <tomas.bonnedahl@trioptima.com>
2018-11-26 21:38:02 +0100

    Move DEBUG

--- sasawards/settings.py
+++ sasawards/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DJANGO_DEBUG']
 
 # TODO: Why was this not needed in the getting-started?
 ALLOWED_HOSTS = ['*']
