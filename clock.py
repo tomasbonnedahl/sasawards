bad1026944d1b55d21b83e968934d0e03361c74f
Tomas Bonnedahl <tomas.bonnedahl@trioptima.com>
2018-11-27 22:14:55 +0100

    Run hourly

--- clock.py
+++ clock.py
@@ -6,7 +6,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=1)  # TODO: Temp
+@sched.scheduled_job('interval', minutes=60)
 def timed_job():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sasawards.settings")
     django.setup()
