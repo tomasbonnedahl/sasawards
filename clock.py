b735d4bb5f2567c7831c443fe4ab5f25df2b20f8
Tomas Bonnedahl <tomas.bonnedahl@trioptima.com>
2018-11-27 20:54:10 +0100

    Run django.setup()

--- clock.py
+++ clock.py
@@ -1,20 +1,28 @@
 import os
 
-from apscheduler.schedulers.background import BackgroundScheduler
+# from apscheduler.schedulers.background import BackgroundScheduler
 
-sched = BackgroundScheduler()
+# sched = BackgroundScheduler()
+import django
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+# TODO: Comment
+sched = BlockingScheduler()
 
 
 @sched.scheduled_job('interval', minutes=1)  # TODO: Temp
 def timed_job():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sasawards.settings")
+    django.setup()
 
     import django_rq
     from rq.worker import logger as rq_logger
     from sas_api.services import get_new_flight_data
 
+    rq_logger.info('Job is executing!')
     queue = django_rq.get_queue()
     if queue.is_empty():
+        rq_logger.info('Enqueuing')
         django_rq.enqueue(get_new_flight_data, rq_logger)
 
     # worker = django_rq.get_worker()
--- sasawards/wsgi.py
+++ sasawards/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from apscheduler.schedulers.background import BackgroundScheduler
+# from apscheduler.schedulers.background import BackgroundScheduler
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sasawards.settings')
