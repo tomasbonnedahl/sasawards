0076db551ee2ee0e3437e1cb51bcc5277fee7658
Tomas Bonnedahl <tomas.bonnedahl@trioptima.com>
2018-12-02 19:59:17 +0100

    First prod run

--- conf.py
+++ conf.py
@@ -1,20 +1,20 @@
 # TODO: Add these to the database and have an interface for it?
 
 MIN_DATE = '20191002'
-MAX_DATE = '20191103'  # Inclusive, could be None
+MAX_DATE = '20191015'  # Inclusive, could be None
 # MAX_DATE=None
 
 DAYS_AHEAD = 340
 
 ORIGINS = [
-    # 'ARN',
-    'CPH'
+    'ARN',
+    # 'CPH'
 ]
 
 DESTINATIONS = [
-    'PVG',
-    # 'EWR',
+    # 'PVG',
+    'EWR',
     # 'NON'
 ]
 
-SECONDS_BETWEEN_REQUESTS = 1
+SECONDS_BETWEEN_REQUESTS = 10
